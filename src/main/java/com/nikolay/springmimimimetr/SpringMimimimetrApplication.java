730abbf26d1ce17aa7a279c4bfd34fbf9d9cb26a
master-kot <master-kot@mail.ru>
2020-03-21 21:58:17 +0300

    Initial commit

---
+++ src/main/java/com/nikolay/springmimimimetr/SpringMimimimetrApplication.java
@@ -0,0 +1,13 @@
+package com.nikolay.springmimimimetr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMimimimetrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMimimimetrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nikolay/springmimimimetr/SpringMimimimetrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikolay.springmimimimetr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMimimimetrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
