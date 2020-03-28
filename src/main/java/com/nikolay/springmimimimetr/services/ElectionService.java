23860b2b36d7480bbe8ba17e391b7bf3336be2f7
master-kot <master-kot@mail.ru>
2020-03-28 17:03:26 +0300

    Small changes of the interface were done

--- src/main/java/com/nikolay/springmimimimetr/services/ElectionService.java
+++ src/main/java/com/nikolay/springmimimimetr/services/ElectionService.java
@@ -7,9 +7,7 @@ import com.nikolay.springmimimimetr.entities.Vote;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 @Service
 public class ElectionService {
@@ -40,20 +38,19 @@ public class ElectionService {
      * когда найдены и отправлены в список для просмотра оба случайных кандидата
      */
     public List<Candidate> showCandidates(User user) {
-        List<Candidate> candidates;
+        List<Candidate> candidates = new ArrayList<>();
         List<Candidate> allCandidates = candidateService.getAllCandidates();
         int numberOfCandidates = allCandidates.size();
+
         List<View> previousViews = viewService.findAllByUser(user);
         if (previousViews.size() < numberOfCandidates) {
             List<View> nextViews = new ArrayList<>();
-            candidates = new ArrayList<>();
             while (candidates.size() < 2) {
                 Candidate randomCandidate = allCandidates.get(randomise.nextInt(numberOfCandidates));
-                /**
-                 * Защита от повторного просмотра кандидатов.
-                 * Применено сравнение по списку просмотров вместо запроса просмотров в БД
+                /* Защита от повторного просмотра кандидатов.
+                 * Применено сравнение по списку просмотров вместо повторения запросов просмотров,
                  * т.к. просмотров может быть не более количества кандидатов,
-                 * и это сработает быстрее чем запрос в БД
+                 * то это работает быстрее, чем каждую итерацию делать новый запрос через сервис.
                 */
                 if (findViewByCandidate(randomCandidate, previousViews) == null
                         && findViewByCandidate(randomCandidate, nextViews) == null) {
@@ -63,11 +60,11 @@ public class ElectionService {
             }
             viewService.saveAllViews(nextViews);
         } else {
-            /**
-             * Сортируем список всех кандидатов пузырьком.
-             * Не нужно сортировать, если массив приходит с БД в уже отсортированном виде
+            /* Поскольку список приходит с БД в неотсортированном виде,
+             * сортируем его в обратном порядке пузырьком либо методом sort.
              */
-            for (int i = 0; i < allCandidates.size(); i++) {
+            allCandidates.sort(Collections.reverseOrder(Comparator.comparing(obj -> obj.getVotes().size())));
+            /*for (int i = 0; i < allCandidates.size(); i++) {
                 for (int j = 0; j < allCandidates.size() - i - 1; j++) {
                     if(allCandidates.get(j).getVotes().size() < allCandidates.get(j+1).getVotes().size()) {
                         Candidate tmp = allCandidates.get(j);
@@ -75,8 +72,14 @@ public class ElectionService {
                         allCandidates.set(j+1, tmp);
                     }
                 }
+            }*/
+            /* Первые 10 кандидатов из списка любого размера также могут быть извлечены непосредственно
+             * при обработке страницы с помощью Thymeleaf методом th:if="${candidates.indexOf(candidate) < 10}"
+             * тогда вместо следующих трех строк присваиваем значение: candidates = allCandidates.
+             */
+            for (int i = 0; i < 10; i++) {
+                candidates.add(allCandidates.get(i));
             }
-            candidates = allCandidates;
         }
         return candidates;
     }
@@ -90,8 +93,9 @@ public class ElectionService {
 
     public void voteForCandidate(User user, Integer id) {
         Candidate candidate = candidateService.getCandidateById(id);
-        //защита от повторного голосования и использования
-        //неправильных ссылок на несуществующего кандидата
+        /*защита от повторного голосования и использования
+         *неправильных ссылок на несуществующего кандидата
+         */
         if (voteService.findOneByCandidateAndUser(candidate, user) == null
                 && id > 0 && id <= candidateService.getNumberOfCandidates()) {
             voteService.saveVote(new Vote(user, candidate));
